29c9c0c7aa7f94bee1b791ae6279a9282e81b14c
Nalthyr <114312397+DanBlash@users.noreply.github.com>
2023-05-26 13:13:05 -0400

    More work done for Coding Assignment week 12

--- Week12-unit-test-assignment/src/main/java/TestDemo.java
+++ Week12-unit-test-assignment/src/main/java/TestDemo.java
@@ -1,7 +1,32 @@
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.Random;
 
 public class TestDemo {
 
+	public int randomNumberSquared() {
+		int result = getRandomInt();
+		return result * result;
+	}
 	
+	
+	
+	
+	
+	
+	@VisibleForTesting
+	int getRandomInt() {
+		Random random = new Random();
+		
+		return random.nextInt(10) + 1;
+	}
+
+
+
+
+
+
+
 	public int addPositive(int a, int b) {
 		if (a > 0 || b > 0) {
 			return a + b;
